21fea92c8e50c6b4270cd68aaa9d874fe6f6cb63
guneysogutt <guneysogut.10@gmail.com>
2025-05-20 18:34:30 +0300

    add docker-compose

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 from application.config import Config
-from application.database.create_database import main as create_database
 
 def create_app(config_class=Config):
-    #create_database()
     app = Flask(__name__)
     CORS(app)
     app.config.from_object(config_class)
@@ -13,4 +11,4 @@ def create_app(config_class=Config):
     from application.api import bp as api_bp
     app.register_blueprint(api_bp)
 
-    return app
+    return app 
